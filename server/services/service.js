25a4407a989fd335dce672fc6ede48a9fbfe40aa
JorgeToAn <jorgetosta.f@gmail.com>
2024-03-03 22:39:37 -0800

    Manejo de ID no encontrado en soap

--- server/services/service.js
+++ server/services/service.js
@@ -7,20 +7,24 @@ const service = {
       getUser: async function (args) {
         const id = args.id;
         const user = await User.findById(id);
-
-        const result = {
-          user: {
-            nombre: user.nombre,
-            apellido: user.apellido,
-            email: user.email,
-            imgRuta: user.imgRuta,
-            ubicacion: user.ubicacion,
-            ocupacion: user.ocupacion,
-            vistasPerfil: user.vistasPerfil,
-            impresiones: user.impresiones
+        if (user) {
+          const result = {
+            user: {
+              nombre: user.nombre,
+              apellido: user.apellido,
+              email: user.email,
+              imgRuta: user.imgRuta,
+              ubicacion: user.ubicacion,
+              ocupacion: user.ocupacion,
+              vistasPerfil: user.vistasPerfil,
+              impresiones: user.impresiones
+            }
           }
-        };
-        return result;
+          return result;
+        } else {
+          return { message: 'No user found' };
+        }
+
       },
       getFriends: async function (args) {
         const id = args.id;
@@ -31,12 +35,12 @@ const service = {
           );
           const formattedFriends = friends.map(
             ({ id, nombre, apellido, ocupacion, ubicacion, imgRuta }) => {
-              return { id, nombre, apellido, ocupacion, ubicacion, imgRuta };
+              return { user:{ id, nombre, apellido, ocupacion, ubicacion, imgRuta } };
             }
           );
           return formattedFriends;
         } else {
-          return { friends: []};
+          return { message: 'No user found'};
         }
       }
     }
