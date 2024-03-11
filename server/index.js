c35591efb703b5c2397ff3dc9e9e78795d6e0bd8
PabloMartinezA <99064319+PabloMartinezA@users.noreply.github.com>
2024-03-10 23:35:35 -0700

    Modificacion a las rutas para paridad con las traducciones

--- server/index.js
+++ server/index.js
@@ -17,9 +17,6 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/Usuarios.js";
 import Post from "./models/Publicaciones.js";
 import { users, posts } from "./data/index.js";
-import soap from "soap";
-import { readFileSync } from "fs";
-import service from "./services/service.js";
 
 /* CONFIGURACIONES */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,9 +32,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* WSDL */
-const xml = readFileSync('service.wsdl', 'utf8');
-
 /* ALMACENAMIENTO DE ARCHIVOS */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -51,13 +45,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* RUTAS CON ARCHIVOS */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/auth/register", upload.single("img"), register);
+app.post("/publicaciones", verifyToken, upload.single("img"), createPost);
 
 /* RUTAS */
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
+app.use("/usuarios", userRoutes);
+app.use("/publicaciones", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
@@ -67,10 +61,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => {
-      soap.listen(app, '/soap', service, xml, () => console.log('Servidor SOAP escuchando en localhost:3001/soap'));
-      console.log(`Server Port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server Port ${PORT}`));
 
     /* INICIALIZACION DE DATOS */
     //User.insertMany(users);
